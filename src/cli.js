399ad025b2a004c9133d1e3f0180a1535d416b1e
Vladislav Makushenko <v.makushenko@uptrader.io>
2024-07-03 18:11:31 +0300

    Make available user greeting via console

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const getUserName = () => {
+    return readlineSync.question('May I have your name? ');
+};
+
+export default () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = getUserName();
+    const greeting = `Hello, ${userName}!`;
+    console.log(greeting);
+};
