31dbd7371cefb3e63b9b4484954a7675ed1269bd
Vladislav Makushenko <v.makushenko@uptrader.io>
2024-07-16 01:21:46 +0300

    Implement brain-prime game

---
+++ src/games/brain-prime.js
@@ -0,0 +1,15 @@
+import getRandomNum from '../utils/getRandomNum.js';
+import isPrime from '../utils/isPrime.js';
+import playGame from '../index.js';
+
+const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const initGameRound = () => {
+  const randomNum = getRandomNum();
+  const question = `${randomNum}`;
+  const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
+
+  return { question, correctAnswer };
+};
+
+export default () => playGame(gameRules, initGameRound);
